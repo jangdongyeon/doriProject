157873e8e5d38477185778e5a7a31b8eca3d6376
dongyeon.jang <zhemaud111@gmail.com>
2024-11-17 00:34:56 +0900

    초기설정

---
+++ dorisns_back/src/main/java/dori/sns/back/DorisnsBackApplication.java
@@ -0,0 +1,13 @@
+package dori.sns.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DorisnsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DorisnsBackApplication.class, args);
+	}
+
+}
---
+++ dorisns_back/src/test/java/dori/sns/back/DorisnsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package dori.sns.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DorisnsBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
